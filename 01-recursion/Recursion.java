42da0502bcd1bcbaf886f68ba5a4cb7895b5239e
Bernadette Baroi <bbaroi30@stuy.edu>
2022-02-03 11:47:43 -0500

    new file

---
+++ 01-recursion/Recursion.java
@@ -0,0 +1,45 @@
+public class Recursion {
+
+  /*Print all words that are made of the letters a-e inclusive.
+  *@param length : the length of the words that are to be printed
+  */
+  public static void printAllWords(int length){
+
+  }
+
+  /*Print all words that are made of the letters a-e inclusive.
+  *@param length : either how many more letters or the total length depending on implementation
+  *@param word   : the partial word so far.
+  */
+  public static void printAllWords(int length,String word){
+    if (size > 0) {
+      for (char c = 'a'; c <= 'e'; c++) {
+        makeWord(size-1,word+c);
+      }
+    } else {
+      System.out.println(word);
+    }
+  }
+
+  /*Print all words that are made of the characters in the array of letters.
+  *There may not be consecutive equal letters, so
+  *aax is not allowed, but axa is allowed.
+  *@param length : the length of the words that are to be printed
+  *@param letters: the letters you should be using,
+  *@precondition: letters contains at least 2 characters, and has no duplicates.
+  */
+  public static void printNoDoubleLetterWords(int length,char[] letters){
+    printNoDoubleLetterWords(length,"",letters);
+  }
+
+  /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+  *aax is not allowed, but axa is allowed.
+  *@param length : either how many more letters need to be
+  *@param word   : the partial word so far.
+  *@param letters: the letters you should be using
+  */
+  public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+      //WRITE THIS METHOD
+  }
+
+}
