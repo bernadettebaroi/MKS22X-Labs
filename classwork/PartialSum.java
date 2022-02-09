0e1576f5883c172b7e092d1342a92dbeddf1ab4f
Bernadette Baroi <bbaroi30@stuy.edu>
2022-02-09 11:45:50 -0500

    RecursionClasswork finished groupSum and tested it

--- classwork/PartialSum.java
+++ classwork/PartialSum.java
@@ -1,16 +1,18 @@
 public class PartialSum {
   public static void main(String[] args) {
-    System.out.println(groupSum(0, [2, 4, 8], 10));
-    System.out.println(groupSum(0, [2, 4, 8], 14));
-    System.out.println(groupSum(0, [2, 4, 8], 9));
+    int[] list = {2,4,8};
+    System.out.println(groupSum(0, list, 10));
+    System.out.println(groupSum(0, list, 14));
+    System.out.println(groupSum(0, list, 9));
   }
 
-  public boolean groupSum(int start, int[] nums, int target) {
-    if (start > nums.length-1 || target == 0) {
+  public static boolean groupSum(int start, int[] nums, int target) {
+    if (start > nums.length-1 && target != 0) {
+      return false;
+    } else if(target == 0) {
       return true;
     } else {
-      target = target - nums[start];
-      return groupSum(start+1,nums,target);
+      return (groupSum(start+1,nums,target) || groupSum(start+1, nums, target-nums[start]));
     }
   }
 
