dcb76591ba079413ab496485458efbb1c463cf0f
Bernadette Baroi <bbaroi30@stuy.edu>
2022-02-10 11:22:59 -0500

    RecursionClasswork some testcases work

--- classwork/PartialSum.java
+++ classwork/PartialSum.java
@@ -7,13 +7,28 @@ public class PartialSum {
   }
 
   public static boolean groupSum(int start, int[] nums, int target) {
-    if (start > nums.length-1 && target != 0) {
-      return false;
-    } else if(target == 0) {
+    if (target == 0) {
       return true;
+    } else if(start >= nums.length) {
+      return false;
     } else {
       return (groupSum(start+1,nums,target) || groupSum(start+1, nums, target-nums[start]));
     }
   }
+
+  public boolean groupSum6(int start, int[] nums, int target) {
+    if (target == 0) {
+      return true;
+    } else if(start >= nums.length) {
+      return false;
+    } else {
+      if (nums[start] == 6) {
+        return (groupSum6(start+1,nums,target) || groupSum6(start+1, nums, target-nums[start]));
+      } else {
+        return (groupSum6(start+1,nums,target) || groupSum6(start+1, nums, target-nums[start]));
+      }
+    }
+  }
+
 
 }
