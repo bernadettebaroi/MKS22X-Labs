a659d9ed56fffcfcdb32a84b518cc53ed2efd171
Bernadette Baroi <bbaroi30@stuy.edu>
2022-03-04 11:48:58 -0500

    still cant figure it out

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -7,9 +7,7 @@ public class MazeGenerator {
         maze[i][j] = '#';
       }
     }
-
-    randomizer(maze,startrow,startcol,0);
-
+    randomizer(maze,startrow,startcol);
     for (int i = 0; i < maze.length;i++) {
       for (int j = 0;j < maze[0].length;j++) {
         System.out.print(maze[i][j]);
@@ -19,36 +17,41 @@ public class MazeGenerator {
 
   }
 
-  public static int randomizer(char[][]maze, int row,int col, int s) {
-    if (emptySpace(maze,row,col)) {
-      return 1;
-    } else if (s > 5) {
-      maze[row][col] = 'E';
-      return 0;
-    } else {
-      if (s == 0) {
-        maze[row][col] = 'S';
-      }
-      s+=1;
-      maze[row][col] = ' ';
-      int random = randomizer(maze, row+1, col,s);
-      if (random > -1) {
-        maze[row+1][col] = ' ';
-      }
-      int random1 = randomizer(maze, row-1, col,s);
-      if (random1 > -1) {
-        maze[row-1][col] = ' ';
-      }
-      int random2 = randomizer(maze, row, col+1,s);
-      if (random2 > -1) {
-        maze[row][col+1] = ' ';
-      }
-      int random3 = randomizer(maze, row, col-1,s);
-      if (random3 > -1) {
-        maze[row][col-1] = ' ';
-      }
+  public static int randomizer(char[][]maze, int row,int col) {
+    if (!emptySpace(maze,row,col)) {
       return -1;
+    } else {
+      int random = (int)(Math.random() * 4 + 1);
+      if (random == 0 ) {
+        maze[row][col] = ' ';
+        if (randomizer(maze, row+1,col) == -1) {
+          randomizer(maze, row-1,col);
+        }
+        randomizer(maze, row+1,col);
+        return 0;
+      } else if (random == 1) {
+        maze[row][col] = ' ';
+        if (randomizer(maze, row-1,col) == -1) {
+          randomizer(maze, row+1,col);
+        }
+        randomizer(maze,row-1,col);
+        return 0;
+      } else if (random == 2) {
+        maze[row][col] = ' ';
+        if (randomizer(maze, row, col+1) == -1) {
+          randomizer(maze, row,col-1);
+        }
+        randomizer(maze, row, col+1);
+      } else {
+        maze[row][col] = ' ';
+        if (randomizer(maze, row, col-1) == -1) {
+          randomizer(maze, row,col+1);
+        }
+        randomizer(maze, row,col-1);
+        return 0;
+      }
     }
+    return 0;
   }
 
   public static boolean emptySpace(char[][]maze, int row, int col) {
