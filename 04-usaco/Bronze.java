09b1c68dd3dfcc7d6b9e5c2da01a4bc0e52082da
Bernadette Baroi <bbaroi30@stuy.edu>
2022-03-09 11:47:40 -0500

    new lab

---
+++ 04-usaco/Bronze.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Bronze {
+  private int[][]maze;
+
+  public static long solve(String filename) {
+
+  }
+  public Bronze() {
+    
+  }
+
+  public static cowStomp() {
+
+  }
+}
---
+++ 04-usaco/Silver.java
@@ -0,0 +1,7 @@
+import java.util.*;
+import java.io.*;
+public class Silver {
+  public static long solve(String filename) {
+
+  }
+}
