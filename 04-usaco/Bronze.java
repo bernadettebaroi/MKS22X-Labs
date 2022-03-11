1c83ad1e3852cd9ddb45c077cdf44b380ffa7286
Bernadette Baroi <bbaroi30@stuy.edu>
2022-03-11 11:46:25 -0500

    something is working

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -1,33 +1,30 @@
 import java.util.*;
 import java.io.*;
 public class Bronze {
-  private int[][]maze;
+  private int[][]board;
   private int R, C, elev_rc, R_s, C_s, D_s, N, E;
-
-  public static Bronze(int row, int col) {
-    maze[][] = maze[row][col];
-    R = row;
-    C = col;
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < col;j++) {
-        int z = Math.random() * 40;
-        maze[row][col] = z;
-      }
-    }
-    setElevRc(maze);
+  public static void main(String[] args) {
+    System.out.println();
   }
-  public static setElevRc() {
+  public static Bronze(int row, int col) {
 
+  }
+
+  public static int setElevRc() {
+    return 0;
   }
 
   public static long solve(String filename) {
-
+    File text = new File(filename);
+    Scanner input = new Scanner(text);
+    int k = 0;
+    ArrayList<int> samp = new ArrayList<int>();
+    int row = input.nextInt()-1;
+    int col = input.nextInt()-1;
+    board = new[row][col];
   }
-  public Bronze() {
 
-  }
-
-  public static cowStomp() {
-
+  public static int cowStomp() {
+    return 0;
   }
 }
