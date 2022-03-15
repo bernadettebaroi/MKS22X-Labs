83c1f1f231ccef23d7aeaa3b33caf910a785bcea
Bernadette Baroi <bbaroi30@stuy.edu>
2022-03-14 23:33:26 -0400

    finally done

--- 04-usaco/Driver.java
+++ 04-usaco/Driver.java
@@ -6,9 +6,16 @@ public class Driver {
       long ans2 = Bronze.solve("makelake.2.in");
       System.out.println(ans2);
       System.out.println("102762432");
-      /*long ans3 = Silver.solve("ctravel.1.in");
+      long ans3 = Silver.solve("ctravel.1.in");
       System.out.println(ans3);
+      System.out.println("1");
       long ans4 = Silver.solve("ctravel.2.in");
-      System.out.println(ans4); */
+      System.out.println(ans4);
+      System.out.println("74");
+      long ans5 = Silver.solve("ctravel.3.in");
+      System.out.println(ans5);
+      System.out.println("8");
+      long ans6 = Silver.solve("ctravel.4.in");
+      System.out.println(ans6);
   }
 }
--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -5,66 +5,44 @@ public class Silver {
   private static int[][]board1;
   private static int[][]board2;
   private static int N, M, T, R1, C1, R2, C2;
-  public static void main(String[] args) throws FileNotFoundException{
-    String name = "ctravel.2.in";
-    solve(name);
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M;j++) {
-        System.out.print(board[i][j] + " ");
-      }
-      System.out.print("\n");
-    }
-    System.out.print("\n");
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M;j++) {
-        System.out.print(board1[i][j] + " ");
-      }
-      System.out.print("\n");
-    }
-    System.out.print("\n");
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M;j++) {
-        System.out.print(board2[i][j] + " ");
-      }
-      System.out.print("\n");
-    }
-    System.out.print("\n");
-    System.out.println(board1[R2][C2]);
-  }
-
-  public static long solve(String filename)throws FileNotFoundException{
-    File text = new File(filename);
-    Scanner input = new Scanner(text);
-    N = input.nextInt();
-    M = input.nextInt();
-    T = input.nextInt();
-    board = new int[N][M];
-    board1 = new int[N][M];
-    board2 = new int[N][M];
-    int i = 0;
-    while (input.hasNext() && i < N) {
-      String g = input.next();
-      for (int j = 0; j < M; j++) {
-        if (g.charAt(j) == '*') {
-          board[i][j] = -1;
-          board1[i][j] = -1;
-          board2[i][j] = -1;
-        } else {
-          board[i][j] = 0;
-          board1[i][j] = 0;
-          board2[i][j] = 0;
+  public static long solve(String filename){
+    try {
+      File text = new File(filename);
+      Scanner input = new Scanner(text);
+      N = input.nextInt();
+      M = input.nextInt();
+      T = input.nextInt();
+      board = new int[N][M];
+      board1 = new int[N][M];
+      board2 = new int[N][M];
+      int i = 0;
+      while (input.hasNext() && i < N) {
+        String g = input.next();
+        for (int j = 0; j < M; j++) {
+          if (g.charAt(j) == '*') {
+            board[i][j] = -1;
+            board1[i][j] = -1;
+            board2[i][j] = -1;
+          } else {
+            board[i][j] = 0;
+            board1[i][j] = 0;
+            board2[i][j] = 0;
+          }
         }
+        i++;
       }
-      i++;
+      R1 = input.nextInt() -1;
+      C1 = input.nextInt() -1;
+      R2 = input.nextInt() -1;
+      C2 = input.nextInt() -1;
+      board[R1][C1] = 1;
+      board1[R1][C1] = 1;
+      go();
+      return board[R2][C2];
+    } catch (FileNotFoundException e) {
+      System.out.println("ERROR: FileNotFoundException");
+      return -1;
     }
-    R1 = input.nextInt() -1;
-    C1 = input.nextInt() -1;
-    R2 = input.nextInt() -1;
-    C2 = input.nextInt() -1;
-    board[R1][C1] = 1;
-    board1[R1][C1] = 1;
-    go();
-    return board[R2][C2];
   }
 
   public static void go() {
