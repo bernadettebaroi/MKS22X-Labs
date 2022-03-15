d4b880ea9b023c065ca92bb599df0a2d4565ca3f
Bernadette Baroi <bbaroi30@stuy.edu>
2022-03-15 11:46:37 -0400

    new lab

---
+++ 05-quick/Preliminary.java
@@ -0,0 +1,17 @@
+public class Preliminary {
+  public static void main(String[]args){
+
+  }
+  /*Modify the array as per directions above.
+  *@return the index of the final position of the pivot element.
+  */
+  public static int partition ( int [] data, int start, int end){
+    int pivotIndex = (int)(Math.random() * end + start);
+    int P = data[pivotIndex];
+    int[] scartch = new int[start-end+1];
+    int i = start;
+    int j = end;
+
+
+  }
+}
